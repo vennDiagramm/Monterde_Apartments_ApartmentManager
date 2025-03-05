3625164b8e9ba9641ba64d890a67f6e7fe38b5ce
ven <margsmay23@gmail.com>
2025-03-05 17:58:39 +0800

    added error handling sa db.js

--- server/db.js
+++ server/db.js
@@ -4,11 +4,21 @@ const pool = mysql.createPool({
     host: 'localhost', // or your MySQL server IP
     user: 'root', // or another MySQL user
     password: '',
-    database: 'db_dorm',
+    database: 'db_dorm', // change on what name is your database
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
+// Error handling for the pool connection
+pool.on('connection', (connection) => {
+    console.log('Database connection established');
+});
+
+pool.on('error', (err) => {
+    console.error('Unexpected database error', err);
+});
+
 // Use pool.promise() for async queries
 module.exports = pool.promise();
+
